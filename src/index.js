6fccdc959343eb292203ed34b5447bf0dfa4eab0
Manish Sharma <mrsharma98@gmail.com>
2024-06-05 12:20:41 +0530

    Custom Response and Error class, standard middlewares.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+    .then(() => {
+        app.on("error", (err) => {
+            console.log("Express initialization failed !!! ", err);
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is runnng at port: ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO db connection failed !!! ", err);
+    })
 
 
 
